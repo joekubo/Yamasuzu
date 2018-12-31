c8998eab42dc7fe114873c5152f0eb70b7f3985c
joe_tolah <joe.tolah@gmail.com>
2018-12-31 15:42:25 +0300

    commit salespanel

--- src/yamasuzu_myshopsoft/SalesPanel.java
+++ src/yamasuzu_myshopsoft/SalesPanel.java
@@ -1,7 +1,5 @@
 package yamasuzu_myshopsoft;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
@@ -13,8 +11,6 @@ import java.sql.PreparedStatement;
 import java.util.Calendar;
 import javax.swing.JDialog;
 import javax.swing.JTextField;
-import javax.swing.SwingWorker;
-import javax.swing.Timer;
 
 public class SalesPanel extends javax.swing.JPanel {
     Connection conn = null;
